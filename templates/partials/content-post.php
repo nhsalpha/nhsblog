0ba038ce33d62a5ec00966e0f4bba95380c689e2
Dominic Baggott <dominic.baggott@gmail.com>
2015-12-18 14:34:32 +0000

    Link author names to author pages

--- templates/partials/content-post.php
+++ templates/partials/content-post.php
@@ -6,9 +6,9 @@
       <div class="entry-meta date">
         <?php
           if(function_exists('coauthors')) {
-            coauthors();
+            coauthors_posts_links();
           } else {
-            the_author();
+            the_author_posts_link();
           }
         ?>,
         <time class="published" datetime="<?php echo get_the_time('c') ?>"><?php echo the_time('F jS Y') ?></time>
